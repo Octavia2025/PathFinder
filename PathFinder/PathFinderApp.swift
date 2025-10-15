ae2e2d9d207cde8f0207da2cd7d35029c4e959bf
Octavia <octaviatmucheche@gmail.com>
2025-10-15 09:24:02 +0200

    Initial Commit

---
+++ PathFinder/PathFinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  PathFinderApp.swift
+//  PathFinder
+//
+//  Created by Octavia Mucheche on 10/15/25.
+//
+
+import SwiftUI
+
+@main
+struct PathFinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
