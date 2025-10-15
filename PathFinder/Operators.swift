b8dc550049d310762ca76ec01e08d13e4784655c
Octavia <octaviatmucheche@gmail.com>
2025-10-15 14:44:57 +0200

    Called out struct operators which will take me to the operators page

---
+++ PathFinder/Operators.swift
@@ -0,0 +1,18 @@
+//
+//  Operators.swift
+//  PathFinder
+//
+//  Created by Octavia Mucheche on 10/15/25.
+//
+
+import SwiftUI
+
+struct Operators: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Operators()
+}
