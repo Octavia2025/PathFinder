3924b22240096462b1b1e8008f6e74d086cdb613
Octavia2025 <octaviatmucheche@gmail.com>
2026-03-24 15:27:13 +0200

    updated

--- PathFinder/ExraCityServices.swift
+++ PathFinder/ExraCityServices.swift
@@ -5,100 +5,10 @@
 //  Created by Octavia Mucheche on 10/15/25.
 //
 
-//import SwiftUI
-//
-//struct ExtraCityServicesTable: Identifiable {
-//    let id: UUID = UUID()
-//    var route: String
-//    var time: String
-//    var fare: String
-//}
-//
-//struct ExtraCityServices: View {
-//    
-//    
-//    @State private var searchText: String = ""
-//    
-//    private var servicesTable: [ExtraCityServicesTable] = [
-//        ExtraCityServicesTable(route: "Harare to Victoria Falls", time: "6:30am, 9am, 5pm, 5:30pm", fare: "$30.00"),
-//        ExtraCityServicesTable(route: "Byo to Victoria Falls", time: "9:00 am, 11:00 am, 2:00 pm, 5:00 pm, 11:30 pm, 12:30 am", fare: "$15.00"),
-//        ExtraCityServicesTable(route: "Hwange to Harare", time: "8:00 am, 9:00 pm, 9:30 pm", fare: "$25.00"),
-//        ExtraCityServicesTable(route: "Vic Falls to Harare", time: "6:30 am, 8:00 pm, 10:30 pm", fare: "$25.00"),
-//        ExtraCityServicesTable(route: "Byo to Harare", time: "1:00am, 3:30 am, 5:00 am, 1:30 pm", fare: "$15.00"),
-//        ExtraCityServicesTable(route: "Gweru to Harare", time: "3:30am, 5:00 am, 7:00 am, 3:00 pm", fare: "$10.00"),
-//        ExtraCityServicesTable(route: "Gweru to Vic Falls", time: "10:30am, 1:00 pm, 9:00 pm, 9:30 pm", fare: "$20.00"),
-//        ExtraCityServicesTable(route: "Lupane to Harare", time: "12:30am, 2:00 am, 10:00 am, 11:00 pm", fare: "$20.00"),
-//        ExtraCityServicesTable(route: "Lupane to Byo", time: "11:00 am", fare: "$10.00"),
-//        ExtraCityServicesTable(route: "Lupane to Vic Falls", time: "2:00am, 2:30 am, 11:00 am, 1:00 pm, 4:00 pm, 7:00 pm", fare: "$10.00"),
-//        ExtraCityServicesTable(route: "Kwekwe to Harare", time: "5:00am, 6:30 am, 8:00 am, 4:00 pm", fare: "$10.00"),
-//        ExtraCityServicesTable(route: "Kwekwe to Vic Falls", time: "9:30am, 12:00 am, 8:00 pm, 8:30 pm", fare: "$10.00"),
-//        ExtraCityServicesTable(route: "Kadoma to Harare", time: "6:30am, 7:30 am, 8:00 am, 9:00 am, 5:00 pm", fare: "$10.00"),
-//        ExtraCityServicesTable(route: "Kadoma to Vic Falls", time: "8:30am, 11:00 am, 7:00 pm, 7:30 pm", fare: "$10.00"),
-//        ExtraCityServicesTable(route: "Chegutu to Harare", time: "7:30am, 8:30 am, 10:00 am, 6:00 pm", fare: "$5.00"),
-//        ExtraCityServicesTable(route: "Chegutu to Vic Falls", time: "7:30am, 10:00 am, 6:00 pm, 6:30 pm", fare: "$20.00")
-//    ]
-//    
-//    // Filtered list based on search text
-//    var filteredServices: [ExtraCityServicesTable] {
-//        if searchText.isEmpty {
-//            return servicesTable
-//        } else {
-//            return servicesTable.filter {
-//                $0.route.localizedCaseInsensitiveContains(searchText) ||
-//                $0.time.localizedCaseInsensitiveContains(searchText) ||
-//                $0.fare.localizedCaseInsensitiveContains(searchText)
-//            }
-//        }
-//    }
-//    
-//    var body: some View {
-//        NavigationStack {
-//            List(filteredServices) { service in
-//                VStack(alignment: .leading, spacing: 6) {
-//                    Text(service.route)
-//                        .font(.headline)
-//                    Text("Time: \(service.time)")
-//                        .font(.subheadline)
-//                    Text("Fare: \(service.fare)")
-//                        .font(.subheadline)
-//                        .foregroundColor(.gray)
-//                }
-//                .padding(.vertical, 5)
-//            }
-//            .navigationTitle("Extra City Services")
-//            .searchable(text: $searchText, prompt: "Search routes, time or fare")
-//            
-//            NavigationStack {
-//                VStack {
-//                    
-//                }
-//                .toolbar {
-//                    ToolbarItem(placement: .navigationBarTrailing) {
-//                        Menu("More") {
-//                            NavigationLink("Office Location") {
-//                                ZimbabweOfficesMapView()
-//                            }
-//                            NavigationLink("Travel Extras & Fees") {
-//                                Travel_Extras_and_fees()
-//                            }
-//                        }
-//                    }
-//                }
-//            }
-//
-//        }
-//        
-//    }
-//    
-//}
-//
-//#Preview {
-//    ExtraCityServices()
-//}
 import SwiftUI
 
 // 1. Data Model matching your Supabase Columns
-struct BusSchedule: Identifiable, Codable {
+struct BusSchedules: Identifiable, Codable {
     var id: UUID = UUID() // Internal ID for SwiftUI
     var route: String
     var time: String
@@ -111,35 +21,35 @@ struct ExtraCityServices: View {
     @State private var searchText: String = ""
     
     // 2. All 16 Routes integrated into the new structure
-    private var servicesTable: [BusSchedule] = [
-        BusSchedule(route: "Harare to Victoria Falls", time: "6:30am, 9am, 5pm, 5:30pm", fare: "$30.00", operatorName: "ExtraCity"),
-        BusSchedule(route: "Byo to Victoria Falls", time: "9:00 am, 11:00 am, 2:00 pm, 5:00 pm, 11:30 pm, 12:30 am", fare: "$15.00", operatorName: "ExtraCity"),
-        BusSchedule(route: "Hwange to Harare", time: "8:00 am, 9:00 pm, 9:30 pm", fare: "$25.00", operatorName: "ExtraCity"),
-        BusSchedule(route: "Vic Falls to Harare", time: "6:30 am, 8:00 pm, 10:30 pm", fare: "$25.00", operatorName: "ExtraCity"),
-        BusSchedule(route: "Byo to Harare", time: "1:00am, 3:30 am, 5:00 am, 1:30 pm", fare: "$15.00", operatorName: "ExtraCity"),
-        BusSchedule(route: "Gweru to Harare", time: "3:30am, 5:00 am, 7:00 am, 3:00 pm", fare: "$10.00", operatorName: "ExtraCity"),
-        BusSchedule(route: "Gweru to Vic Falls", time: "10:30am, 1:00 pm, 9:00 pm, 9:30 pm", fare: "$20.00", operatorName: "ExtraCity"),
-        BusSchedule(route: "Lupane to Harare", time: "12:30am, 2:00 am, 10:00 am, 11:00 pm", fare: "$20.00", operatorName: "ExtraCity"),
-        BusSchedule(route: "Lupane to Byo", time: "11:00 am", fare: "$10.00", operatorName: "ExtraCity"),
-        BusSchedule(route: "Lupane to Vic Falls", time: "2:00am, 2:30 am, 11:00 am, 1:00 pm, 4:00 pm, 7:00 pm", fare: "$10.00", operatorName: "ExtraCity"),
-        BusSchedule(route: "Kwekwe to Harare", time: "5:00am, 6:30 am, 8:00 am, 4:00 pm", fare: "$10.00", operatorName: "ExtraCity"),
-        BusSchedule(route: "Kwekwe to Vic Falls", time: "9:30am, 12:00 am, 8:00 pm, 8:30 pm", fare: "$10.00", operatorName: "ExtraCity"),
-        BusSchedule(route: "Kadoma to Harare", time: "6:30am, 7:30 am, 8:00 am, 9:00 am, 5:00 pm", fare: "$10.00", operatorName: "ExtraCity"),
-        BusSchedule(route: "Kadoma to Vic Falls", time: "8:30am, 11:00 am, 7:00 pm, 7:30 pm", fare: "$10.00", operatorName: "ExtraCity"),
-        BusSchedule(route: "Chegutu to Harare", time: "7:30am, 8:30 am, 10:00 am, 6:00 pm", fare: "$5.00", operatorName: "ExtraCity"),
-        BusSchedule(route: "Chegutu to Vic Falls", time: "7:30am, 10:00 am, 6:00 pm, 6:30 pm", fare: "$20.00", operatorName: "ExtraCity")
+    private var servicesTable: [BusSchedules] = [
+        BusSchedules(route: "Harare to Victoria Falls", time: "6:30am, 9am, 5pm, 5:30pm", fare: "$30.00", operatorName: "ExtraCity"),
+        BusSchedules(route: "Byo to Victoria Falls", time: "9:00 am, 11:00 am, 2:00 pm, 5:00 pm, 11:30 pm, 12:30 am", fare: "$15.00", operatorName: "ExtraCity"),
+        BusSchedules(route: "Hwange to Harare", time: "8:00 am, 9:00 pm, 9:30 pm", fare: "$25.00", operatorName: "ExtraCity"),
+        BusSchedules(route: "Vic Falls to Harare", time: "6:30 am, 8:00 pm, 10:30 pm", fare: "$25.00", operatorName: "ExtraCity"),
+        BusSchedules(route: "Byo to Harare", time: "1:00am, 3:30 am, 5:00 am, 1:30 pm", fare: "$15.00", operatorName: "ExtraCity"),
+        BusSchedules(route: "Gweru to Harare", time: "3:30am, 5:00 am, 7:00 am, 3:00 pm", fare: "$10.00", operatorName: "ExtraCity"),
+        BusSchedules(route: "Gweru to Vic Falls", time: "10:30am, 1:00 pm, 9:00 pm, 9:30 pm", fare: "$20.00", operatorName: "ExtraCity"),
+        BusSchedules(route: "Lupane to Harare", time: "12:30am, 2:00 am, 10:00 am, 11:00 pm", fare: "$20.00", operatorName: "ExtraCity"),
+        BusSchedules(route: "Lupane to Byo", time: "11:00 am", fare: "$10.00", operatorName: "ExtraCity"),
+        BusSchedules(route: "Lupane to Vic Falls", time: "2:00am, 2:30 am, 11:00 am, 1:00 pm, 4:00 pm, 7:00 pm", fare: "$10.00", operatorName: "ExtraCity"),
+        BusSchedules(route: "Kwekwe to Harare", time: "5:00am, 6:30 am, 8:00 am, 4:00 pm", fare: "$10.00", operatorName: "ExtraCity"),
+        BusSchedules(route: "Kwekwe to Vic Falls", time: "9:30am, 12:00 am, 8:00 pm, 8:30 pm", fare: "$10.00", operatorName: "ExtraCity"),
+        BusSchedules(route: "Kadoma to Harare", time: "6:30am, 7:30 am, 8:00 am, 9:00 am, 5:00 pm", fare: "$10.00", operatorName: "ExtraCity"),
+        BusSchedules(route: "Kadoma to Vic Falls", time: "8:30am, 11:00 am, 7:00 pm, 7:30 pm", fare: "$10.00", operatorName: "ExtraCity"),
+        BusSchedules(route: "Chegutu to Harare", time: "7:30am, 8:30 am, 10:00 am, 6:00 pm", fare: "$5.00", operatorName: "ExtraCity"),
+        BusSchedules(route: "Chegutu to Vic Falls", time: "7:30am, 10:00 am, 6:00 pm, 6:30 pm", fare: "$20.00", operatorName: "ExtraCity")
     ]
     
     // 3. Search logic
-    var filteredServices: [BusSchedule] {
+    var filteredServices: [BusSchedules] {
         if searchText.isEmpty {
-            return servicesTable
+            return servicesTable 
         } else {
             return servicesTable.filter {
                 $0.route.localizedCaseInsensitiveContains(searchText) ||
                 $0.time.localizedCaseInsensitiveContains(searchText) ||
                 $0.fare.localizedCaseInsensitiveContains(searchText)
-            }
+            } 
         }
     }
     
