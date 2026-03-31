2e3715451905bf42e7d3aca7bea748617f3face5
Octavia2025 <octaviatmucheche@gmail.com>
2026-03-31 12:32:25 +0200

    API key

--- Configuration.swift
+++ Configuration.swift
@@ -10,7 +10,7 @@ import Foundation
 enum Configuration {
     // This looks into your Info.plist for the key you created in Step 3
     static var apiKey: String {
-        guard let token = Bundle.main.object(forInfoDictionaryKey: "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") as? String else {
+        guard let token = Bundle.main.object(forInfoDictionaryKey: "https://github.com/Octavia2025/PathFinder.git") as? String else {
             // This error helps you debug if the Info.plist setup is wrong
             fatalError("GitHub API Key not found in Info.plist. Check your Secrets.xcconfig setup.")
         }
